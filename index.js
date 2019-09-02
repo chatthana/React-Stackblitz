5b538a7c1698dd7bf2076b628d60106b0f0c3b27
chatthana <chattana.j@gmail.com>
2019-09-02 17:41:49 +0700

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Girl from './Girl';
 import FormZone from './FormZone';
+import NotFound from './NotFound';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Store from './store/store';
 // Stylesheet imported here
 import './style.sass';
@@ -12,14 +14,18 @@ const store = Store();
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div className="container">
-          <FormZone />
-          <Girl />
-        </div>
-      </Provider>
+      <Switch>
+        <Route exact path="/" component={Girl} />
+        <Route exact path="/signin" component={FormZone} />
+        <Route component={NotFound} />
+      </Switch>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(
+<Router>
+  <Provider store={store}>
+    <App />
+  </Provider>
+</Router>, document.getElementById('root'));
